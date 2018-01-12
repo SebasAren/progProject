f1486435d3ce199c27ea85cb043be0a341814bc5
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2018-01-12 13:53:17 +0100

    done with drag functionallity

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -26,7 +26,10 @@ $(function() {
 
     $('.datamaps-subunit').mousedown(function(e) {
         dragController($(this), e);
-    })
+        $(document.body).mouseup(function(e) {
+            pickController($(this), e);
+        });
+    });
 }); 
 
 /* Function to control the face of the buttons and how they look.
@@ -41,42 +44,57 @@ var controlButtons = function(type) {
     };
 };
 
+function dragController(clicked, country) {
 
-function dragController(clicked, e) {
+    // movement manager of the new element
     function moveElement(position, element) {
-        console.log(element);
-        //element.css({
-            //'left': position.pageX + 'px',
-            //'top': position.pageY + 'px'
-        //});
         element.offset({
-            left: position.pageX,
-            top: position.pageY
+            'left': position.pageX - element[0].clientWidth / 2,
+            'top': position.pageY - element[0].clientHeight / 2
         });
     }
+    function highlightPlot(position, element) {
+        if (position.pageX > element.offset().left &&
+        position.pageX < (element.offset().left + element.width())
+        && position.pageY > element.offset().top
+        && position.pageY < element.offset().top
+        + element.height()) {
+            element.css({
+                'outline': '#00FF00 dotted thick'
+            });
+        }
+        else {
+            element.css({
+                'outline': 'none' 
+            });
+        }
+    }
+    console.log(country);
     
-    console.log(clicked.attr('class'));
-    console.log(e.pageY, e.pageX);
-    var div = $('<div class="btn"/>')
+    var div = $('<div class="drag-div"/>')
         .css({
-            'left': e.pageX + 'px',
-            'top': e.pageY + 'px',
-            // basically hardcoding the position and 'depth' of the item
-            'position': 'absolute',
-            'z-index': '1000'
+            'left': (country.pageX) + 'px',
+            'top': (country.pageY) + 'px',
         })
         .appendTo($('body'))
         .html('Test')
+        // functions needed to control the new element
         .mousemove(function(event) {
             moveElement(event, $(this));
+            var leftElement = $('.left-plot');
+            var rightElement = $('.right-plot');
+            highlightPlot(event, leftElement);
+            highlightPlot(event, rightElement);
         })
         .mouseleave(function(event) {
             moveElement(event, $(this));
         })
         .mouseout(function(event) {
             moveElement(event, $(this));
-        })
-        .mouseenter(function(event) {
-            moveElement(event, $(this));
         });
+}
+
+function pickController(clicked, country) {
+    console.log('trigger');
+    $('.drag-div').remove();
 }
